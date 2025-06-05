14115197d8c5f6d6b0677a5f7f019981807ee6f9
Joshua Rhodes <joshdr83@gmail.com>
2025-06-05 15:21:23 -0500

    Fix column name check for inertia requirements

--- src/load_inputs/load_inertia_req.jl
+++ src/load_inputs/load_inertia_req.jl
@@ -5,8 +5,9 @@ function load_inertia_req!(setup::Dict, path::AbstractString, inputs::Dict)
         return nothing
     end
     df = load_dataframe(joinpath(path, filename))
+    rename!(df, lowercase.(names(df)))
     validate_df_cols(df, filename, ["mw_s"])
-    inputs["InertiaReq"] = collect(skipmissing(df.MW_s))
+    inputs["InertiaReq"] = collect(skipmissing(df.mw_s))
     println(filename * " Successfully Read!")
     return nothing
 end
